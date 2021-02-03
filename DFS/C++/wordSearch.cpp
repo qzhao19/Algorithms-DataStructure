554a0b7895dfb75a3e94ed4f8e461e1dbeb803cc
zhaoqi19 <qi.zhao1991@gmail.com>
2021-02-03 23:32:53 +0800

    modify

--- DFS/C++/wordSearch.cpp
+++ DFS/C++/wordSearch.cpp
@@ -2,42 +2,41 @@ class Solution {
 private:
     bool is_visited = false;
 public:
-
-    bool dfs(vector<vector<char>> &board, string &word, int cur_idx, int x, int y) {
-        if (word.size() == cur_idx) {
+    bool dfs(vector<vector<char>> &board, string word, int begin, int row, int col) {
+        if (word.size() == begin) {
             is_visited = true;
             return true;
         }
 
-        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] != word[cur_idx]) {
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[begin]) {
             return false;
         }
 
         bool is_visited_top, is_visited_bottom, is_visited_left, is_visited_right;
 
         if (is_visited == false) {
-            char tmp = board[x][y];
-            board[x][y] = '.';
-
-            is_visited_top = dfs(board, word, cur_idx + 1, x - 1, y);
-            is_visited_bottom = dfs(board, word, cur_idx + 1, x + 1, y);
-            is_visited_left = dfs(board, word, cur_idx + 1, x, y - 1);
-            is_visited_right = dfs(board, word, cur_idx + 1, x, y + 1);
-            board[x][y] = tmp;
+            char tmp = board[row][col];
+            board[row][col] = '.';
+            is_visited_top = dfs(board, word, begin + 1, row - 1, col);
+            is_visited_bottom = dfs(board, word, begin + 1, row + 1, col);
+            is_visited_left = dfs(board, word, begin + 1, row, col - 1);
+            is_visited_right = dfs(board, word, begin + 1, row, col + 1);
+            board[row][col] = tmp;
 
             return is_visited_top || is_visited_bottom || is_visited_left || is_visited_right;
         }
-
         return true;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        if (word.size() == 0 || board.size() == 0 || board[0].size() == 0) {
+        int n_rows = board.size(), n_cols = board[0].size();
+
+        if (n_rows == 0 || n_cols == 0 || word.size() == 0) {
             return false;
         }
 
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
+        for (int i = 0; i < n_rows; i++) {
+            for (int j = 0; j < n_cols; j++) {
                 if (dfs(board, word, 0, i, j)) {
                     return true;
                 }
@@ -45,6 +44,5 @@ public:
         }
 
         return false;
-
     }
 };
