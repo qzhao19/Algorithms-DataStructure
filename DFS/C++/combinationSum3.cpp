2e4956b1a0e7803a7aea76df23d97ee613ee86c7
zhaoqi19 <qi.zhao1991@gmail.com>
2021-02-10 23:21:26 +0800

    modify

--- DFS/C++/combinationSum3.cpp
+++ DFS/C++/combinationSum3.cpp
@@ -1,30 +1,26 @@
 class Solution {
 private:
-    vector<int> candidates = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<vector<int>> ans;
     vector<int> path;
+    vector<vector<int>> result;
 
 public:
-
-    void backtrack(int k, int target, int start) {
-        if (target == 0 && k == 0) {
-            ans.push_back(path);
+    void backtrack(vector<int> &candidats, int target, int k, int begin) {
+        if (k == 0 && target == 0) {
+            result.push_back(path);
             return ;
         }
 
-        for (int i = start; i < candidates.size(); i++) {
-            if (target - candidates[i] < 0) {
-                continue;
-            }
-            path.push_back(candidates[i]);
-            backtrack(k - 1, target - candidates[i], i + 1);
+        for (int i = begin; i < candidats.size(); i++) {
+            path.push_back(candidats[i]);
+            backtrack(candidats, target - candidats[i], k - 1, i + 1);
             path.pop_back();
         }
     }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> candidats = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    vector<vector<int>> combinationSum3(int k, int target) {
-        backtrack(k, target, 0);
+        backtrack(candidats, n, k, 0);
 
-        return ans;
+        return result;
     }
 };
