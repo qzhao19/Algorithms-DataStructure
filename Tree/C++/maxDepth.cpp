dc4a3394dca277bca24eec168be076e3087e445c
zhaoqi19 <qi.zhao1991@gmail.com>
2021-02-28 23:05:35 +0800

    modify

--- Tree/C++/maxDepth.cpp
+++ Tree/C++/maxDepth.cpp
@@ -16,3 +16,36 @@ public:
         return max(maxDepth(root -> left), (maxDepth(root -> right))) + 1;
     }
 };
+
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode*> node_queue;
+        int depth = 0;
+
+        if (root != nullptr) {
+            node_queue.push(root);
+        }
+
+        while (!node_queue.empty()) {
+            int q_size = node_queue.size();
+
+            while (q_size--) {
+                TreeNode *cur_node = node_queue.front(); node_queue.pop();
+                if (cur_node -> left != nullptr) {
+                    node_queue.push(cur_node -> left);
+                }
+
+                if (cur_node -> right != nullptr) {
+                    node_queue.push(cur_node -> right);
+                }
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
+};
