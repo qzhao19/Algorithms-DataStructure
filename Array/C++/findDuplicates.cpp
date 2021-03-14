ee006f82b939b172adfd6668e8ad4827a45a1a46
zhaoqi19 <qi.zhao1991@gmail.com>
2021-03-14 23:33:08 +0800

    modify findDuplicates

--- Array/C++/findDuplicates.cpp
+++ Array/C++/findDuplicates.cpp
@@ -22,3 +22,28 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        if (nums.size() < 1) {
+            return {};
+        }
+
+        int n = nums.size();
+        vector<int> result;
+
+        for (int i = 0; i < n; i++) {
+            int abs_num = abs(nums[i]);
+            if (nums[abs_num - 1] > 0) {
+                nums[abs_num - 1] *= (-1);
+            }
+            else {
+                result.push_back(abs_num);
+            }
+        }
+
+        return result;
+    }
+};
