de5fd5ffbcbcf0ed958e9dafc6a8a09796fb8fc4
zhaoqi19 <qi.zhao1991@gmail.com>
2021-03-15 23:31:41 +0800

    modify mergeTwoList

---
+++ LinkList/C++/mergeTwoLists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL || l2 == NULL) {
+            return l1 ? l1 : l2;
+        }
+
+        ListNode *dummy_node = new ListNode(0);
+        ListNode *cur_ptr = dummy_node;
+
+        while (l1 != NULL && l2 != NULL) {
+            if (l1 -> val < l2 -> val) {
+                cur_ptr -> next = l1;
+                l1 = l1 -> next;
+            }
+            else {
+                cur_ptr -> next = l2;
+                l2 = l2 -> next;
+            }
+            cur_ptr = cur_ptr -> next;
+        }
+
+        cur_ptr -> next = (l1 ? l1 : l2);
+
+        return dummy_node -> next;
+
+    }
+};
