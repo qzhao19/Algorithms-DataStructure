304cda7b32465aa7f4baf2e7b3c488cd01b7e5e8
zhaoqi19 <qi.zhao1991@gmail.com>
2021-04-02 00:23:06 +0800

    delete all

--- Array/Python/two_sum2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for indice1, value1 in enumerate(nums):
-            for indice2, value2 in enumerate(nums):
-                if ((value1 + value2) == target) & (indice1 != indice2):
-                    return indice1, indice2
-                    
-            
-        
