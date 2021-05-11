cdf4964ed87894aedab8a2fd1d7870f322ac80e8
zhaoqi19 <qi.zhao1991@gmail.com>
2021-05-11 22:23:22 +0800

    finish list.cpp file

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -3,6 +3,7 @@
 
 template<typename Type>
 void List<Type>::init() {
+
     header = new ListNode<Type>;
     trailer = new ListNode<Type>;
 
@@ -13,7 +14,19 @@ void List<Type>::init() {
     trailer -> succ = header;
 
     _size = 0;
-};
+}
+
+template<typename Type>
+int List<Type>::clear() {
+    int old_size = _size;
+
+    while (_size > 0) {
+        remove(header -> next);
+    }
+
+    return old_size;
+}
+
 
 template<typename Type>
 void List<Type>::copyNodes(ListNodePosition<Type> node, int n) {
@@ -24,6 +37,58 @@ void List<Type>::copyNodes(ListNodePosition<Type> node, int n) {
         node = node -> next;
     }
 }
+//***********************************************************************************//
+
+template<typename Type>
+List<Type>::List() {
+    init();
+}
+
+
+template<typename Type>
+List<Type>::List(List<Type> const &L) {
+    copyNodes(L.first(), L._size());
+}
+
+template<typename Type>
+List<Type>::List(ListNodePosition<Type> node, int n) {
+    copyNodes(node, n);
+}
+
+//***********************************************************************************//
+
+template<typename Type>
+List<Type>::~List() {
+    clear();
+    delete header;
+    delete trailer;
+}
+
+//************************************************************************************//
+template<typename Type>
+ListNodePosition<Type> List<Type>::insertAsFirst(Type const &x) {
+    _size++;
+    return header -> insertAsPrev(x);
+}
+
+template<typename Type>
+ListNodePosition<Type> List<Type>::insertAsLast(Type const & x) {
+    _size++;
+    return trailer -> insertAsNext(x);
+}
+
+
+template<typename Type>
+Type List<Type>::remove(ListNodePosition<Type> node) {
+    Type x = node -> val;
+    node -> prev -> next = node -> next;
+    node -> next -> prev = node -> prev;
+    delete node;
+    _size--;
+
+    return x;
+}
+
 
 
 
