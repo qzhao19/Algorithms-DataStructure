2e439809d8b9d22cba0d9d31728cfc71e67744b1
zhaoqi19 <qi.zhao1991@gmail.com>
2021-05-12 22:04:05 +0800

    fix TreeNode.h

--- BinaryTree/TreeNode.h
+++ BinaryTree/TreeNode.h
@@ -2,16 +2,16 @@
 #define TREENODE_H_
 
 
-template<typename T>
+template<typename T> 
 struct TreeNode {
     T val;
     TreeNode *left;
     TreeNode *right;
 
-    TreeNode(): val(0), left(nullptr), right(nullptr) {}
-    TreeNode(T x): val(x), left(nullptr), right(nullptr) {}
-    TreeNode(TreeNode *node): val(node-> val), left(node -> left), right(node -> right) {}
-    TreeNode(T x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+    TreeNode(): val(0), left(nullptr), right(nullptr) {};
+    TreeNode(T x): val(x), left(nullptr), right(nullptr) {};
+    TreeNode(TreeNode *node): val(node-> val), left(node -> left), right(node -> right) {};
+    TreeNode(T x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {};
 
 };
 
