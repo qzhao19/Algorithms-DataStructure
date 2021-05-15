699599a5305cfc1f3be44e1c5239f6c35b725897
zhaoqi19 <qi.zhao1991@gmail.com>
2021-05-15 22:46:51 +0800

    update binary tree

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -2,7 +2,7 @@
 #include "BinaryTree.h"
 
 template <typename T>
-TreeNode<T>* BinaryTree<T>::copy(const TreeNode<T> *node) {
+TreeNode<T>* BinaryTree<T>::copy(TreeNode<T> *node) {
     if (node == nullptr) {
         return nullptr;
     }
@@ -10,6 +10,51 @@ TreeNode<T>* BinaryTree<T>::copy(const TreeNode<T> *node) {
     return new TreeNode(node-> val, copy(node -> left), copy(node -> right));
 
 }
+
+template <typename T>
+void BinaryTree<T>::clear(TreeNode<T> *node) {
+    if (node != nullptr) {
+        clear(node -> left);
+        clear(node -> right);
+        delete node;
+    }
+
+    node = nullptr;
+}
+
+
+
+template <typename T>
+void BinaryTree<T>::insert(TreeNode<T> *node, const T &x) {
+    if (x < node -> val) {
+        if (node -> left != nullptr) {
+            insert(node -> left, x);
+        }
+        else {
+            node -> left = new TreeNode<T>;
+            node -> left -> val = x;
+            node -> left -> left = nullptr;
+            node -> left -> right = nullptr;
+        }
+    }
+    else if (x >= node -> val) {
+        if (node -> right != nullptr) {
+            insert(node -> right, x);
+        }
+        else {
+            node -> right = new TreeNode<T>;
+            node -> right -> val = x;
+            node -> right -> left = nullptr;
+            node -> right -> right = nullptr;
+        }
+    }
+    
+}
+
+
+
+
+
 
 
 template <typename T>
@@ -30,5 +75,29 @@ template <typename T>
 BinaryTree<T>::BinaryTree(const T &x) {
     root = TreeNode(x);
 }
+
+template <typename T>
+BinaryTree<T>::~BinaryTree() {
+    clear(root);
+}
+
+
+template <typename T>
+void BinaryTree<T>::insert(const T &x) {
+    if (root != nullptr) {
+        insert(root, x);
+    } 
+    else {
+        root = new TreeNode<T>;
+        root -> val = x;
+        root -> left = nullptr;
+        root -> right = nullptr;
+    } 
+
+}
+
+
+
+
 
 
--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -7,16 +7,31 @@
 template<typename T>
 class BinaryTree {
 private:
-    TreeNode *root;
+    TreeNode<T> *root;
 
 protected:
-    TreeNode<T> * copy(const TreeNode<T> *node);
-    void clear();
+    TreeNode<T> * copy(TreeNode<T> *node);
+    void clear(TreeNode<T> *node);
+
+    void insert(TreeNode<T> *node, const T &x);
+    void remove(TreeNode<T> *node, const T &x);
+
+
+
 
 public:
     BinaryTree();
     BinaryTree(const BinaryTree<T> &tree);
     BinaryTree(const T &x);
+
+    ~BinaryTree() {};
+
+    void insert(const T &x);
+    void remove(const T &x);
+
+
+
+
 
 
 
