744fa697a0d3cf9d3fac13966ea8fb027cb1b531
zhaoqi19 <qi.zhao1991@gmail.com>
2021-05-17 20:43:38 +0800

    update binary tree header file

--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -1,23 +1,24 @@
 #ifndef BINARYTREE_H_
-#define BINARYTREE_H
+#define BINARYTREE_H_
 
 #include "TreeNode.h"
 
 
-template<typename T>
-class BinaryTree {
+template<typename T> class BinaryTree {
 private:
     TreeNode<T> *root;
 
 protected:
-    TreeNode<T> * copy(TreeNode<T> *node);
+    TreeNode<T> *copy(TreeNode<T> *node);
     void clear(TreeNode<T> *node);
 
     void insert(TreeNode<T> *node, const T &x);
     void remove(TreeNode<T> *node, const T &x);
+    TreeNode<T> *search(TreeNode<T> *node, const T &x);
 
-
-
+    void inorderTraversal(TreeNode<T> *node);
+    void postorderTraversal(TreeNode<T> *node);
+    void preorderTraversal(TreeNode<T> *node);
 
 public:
     BinaryTree();
@@ -28,17 +29,14 @@ public:
 
     void insert(const T &x);
     void remove(const T &x);
+    TreeNode<T> search(const T &x);
 
-
-
-
-
+    void inorderTraversal();
+    void postorderTraversal();
+    void preorderTraversal();
 
 
 };
-
-
-
 
 
 
